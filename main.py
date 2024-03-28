88033e9d907beac0c14275cbb78ff070c059b563
Evelyn Suzarte <evelynsuzarte@hotmail.com>
2024-03-27 22:25:59 -0300

    teste 2

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import requests
 import zipfile
+import tabula
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+import pandas as pd
 from bs4 import BeautifulSoup
 import os
 
@@ -30,17 +34,43 @@ def transformar_zip(nomes_arquivos, nome_zip):
     with zipfile.ZipFile(nome_zip,'w') as zip_file:
         for nome in nomes_arquivos:
             if os.path.exists(nome):
-                zip_file.write("Teste1/"+ nome,os.path.basename(nome))
+                zip_file.write(nome,os.path.basename(nome))
                 print(f"Arquivo '{nome}' adicionado ao ZIP.")
             else:
                 print(f"Arquivo '{nome}' não encontrado. Ignorando.")
     print(f"Arquivo ZIP '{nome_zip}' criado com sucesso.")
 
 
+def transformar_csv(nome_arquivo):
+    lista_df = tabula.read_pdf("Teste1/"+nome_arquivo, pages="all", encoding="utf-8")
+    df = pd.concat(lista_df)
+    df.to_csv("Teste2/"+nome_arquivo[:-4]+".csv", index=False)
+
+
+def alterar_palavra(arquivo, palavras_antigas, palavras_novas):
+    df = pd.read_csv("Teste2/"+arquivo)
+    
+    for antiga, nova in zip(palavras_antigas,palavras_novas):
+        df.replace(antiga, nova, inplace=True)
+    
+    df.to_csv("Teste2/corrigido_"+arquivo[:-4]+'.csv', index=False, encoding='utf-8')
+
+
+
+
+    
+
+
 url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
 nomes_arquivos = ["Anexo_I_Rol_2021RN_465.2021_RN599_RN600.pdf","Anexo_II_DUT_2021_RN_465.2021_RN599.pdf"]
-nome_zip = "Anexos.zip"
+nome_zip_teste1 = "Anexos.zip"
+nome_zip_teste2 = "Teste2/Teste_EvelynSuzarteFernandes.zip"
+palavras_antigas = ["OD","AMB"]
+palavras_novas = ["Seg. Odontológica","Seg. Ambulatorial"]
 
 #baixar_arquivosPDF(url,nomes_arquivos)
-transformar_zip(nomes_arquivos, nome_zip)
+#transformar_zip("Teste1/"+nomes_arquivos, nome_zip_teste1)
+#transformar_csv(nomes_arquivos[0])
+#alterar_palavra("Anexo_I_Rol_2021RN_465.2021_RN599_RN600.csv",palavras_antigas, palavras_novas)
+transformar_zip(["Teste2/corrigido_"+nomes_arquivos[0][:-4]+".csv"], nome_zip_teste2)
 
