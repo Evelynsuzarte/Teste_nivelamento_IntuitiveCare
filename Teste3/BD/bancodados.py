49082aada9a20ee6314cfd833d01edc80919a8a9
Evelyn Suzarte <evelynsuzarte@hotmail.com>
2024-03-29 16:03:54 -0300

    banco de dados

--- Teste3/BD/bancodados.py
+++ Teste3/BD/bancodados.py
@@ -1,75 +1,97 @@
 import mysql.connector
 
-conexao = mysql.connector.connect(
-    host="127.0.0.1",
-    user="root",
-    password="",
-    connect_timeout=30
-)
+def tabela_demonstracoes_script(config):
+    conexao = mysql.connector.connect(**config)
+    query = conexao.cursor()
+    query.execute("use testeIntuitiveCare")
+    with open("Teste3/BD/demonstracoes.sql", "r") as script:
+        script_sql = script.read()
+        print(script_sql)
+        script_sql = script_sql.replace('\n', '').strip() 
+    query.execute(script_sql)
+    conexao.commit()
+    query.close()
+    conexao.close()
 
-query = conexao.cursor()
-
-query.execute("CREATE DATABASE IF NOT EXISTS testeIntuitiveCare")
-query.execute("use testeIntuitiveCare")
-
-
-# def estruturar_tabelas():
-#     with open("Teste3/BD/demonstracoes.sql", "r") as script:
-#         script_sql = script.read()
-#         print(script_sql)
-#         script_sql = script_sql.replace('\n', '').strip() 
-#     query.execute(script_sql)
-
-#     with open("Teste3/Relatorio_cadop.csv", "r", encoding="utf-8") as file:
-#         linhas = file.readlines()[1:] 
-#         dados = [linha.strip().split(';') for linha in linhas]
-#         for linha in dados:
-#             for i, dado in enumerate(linha):
-#                 if dado == "":
-#                     linha[i] = '" "'
-#         #dados = [[valor.replace(';;', ' ') for valor in linha] for linha in dados]
-#         #dados = [[valor.replace('"', ' ') for valor in linha] for linha in dados]
-
-#     query_demonstracoes = "INSERT INTO demonstracoes (Registro_ANS, CNPJ, Razao_Social, Nome_Fantasia, Modalidade, Logradouro, Numero, Complemento, Bairro, Cidade, UF, CEP, DDD, Telefone, Fax, Endereco_eletronico, Representante, Cargo_Representante, Regiao_de_Comercializacao, Data_Registro_ANS) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-#     query.executemany(query_demonstracoes, dados)
-#     # for linha in dados:
-#     #     query.execute(f"INSERT INTO demonstracoes (Registro_ANS, CNPJ, Razao_Social, Nome_Fantasia, Modalidade, Logradouro, Numero, Complemento, Bairro, Cidade, UF, CEP, DDD, Telefone, Fax, Endereco_eletronico, Representante, Cargo_Representante, Regiao_de_Comercializacao, Data_Registro_ANS) VALUES ({linha[0]}, {linha[1]}, {linha[2]}, {linha[3]}, {linha[4]}, {linha[5]}, {linha[6]}, {linha[7]}, {linha[8]}, {linha[9]}, {linha[10]}, {linha[11]}, {linha[12]}, {linha[13]}, {linha[14]}, {linha[15]}, {linha[16]}, {linha[17]}, {linha[18]}, {linha[19]})")
-
-
-with open("Teste3/BD/operadoras_ativas.sql", "r") as script:
-    script_sql = script.read()
-    print(script_sql)
-    #script_sql = script_sql.replace('\n', '').strip() 
-query.execute(script_sql)
-
-arquivos = ["1T2022", "2T2022", "3T2022", "4T2022", "1T2023", "2T2023", "3T2023"]
-for nome in arquivos:
-    with open(f"Teste3/{nome}.csv", "r", encoding="latin-1") as file:
+def tabela_demonstracoes_csv(config):
+    conexao = mysql.connector.connect(**config)
+    query = conexao.cursor()
+    query.execute("use testeIntuitiveCare")
+    with open("Teste3/Relatorio_cadop.csv", "r", encoding="utf-8") as file:
         linhas = file.readlines()[1:] 
         dados = [linha.strip().split(';') for linha in linhas]
-        dados = [[valor.replace('"', '') for valor in linha] for linha in dados]
+        for linha in dados:
+            for i, dado in enumerate(linha):
+                if dado == "":
+                    linha[i] = '" "'
+
+    query_demonstracoes = "INSERT IGNORE INTO demonstracoes (Registro_ANS, CNPJ, Razao_Social, Nome_Fantasia, Modalidade, Logradouro, Numero, Complemento, Bairro, Cidade, UF, CEP, DDD, Telefone, Fax, Endereco_eletronico, Representante, Cargo_Representante, Regiao_de_Comercializacao, Data_Registro_ANS) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    query.executemany(query_demonstracoes, dados)
+    conexao.commit()
+    query.close()
+    conexao.close()
+
+#---------------------------------------------------------------------------------
+    
+def tabela_operadorasAtivas_script(config):
+    conexao = mysql.connector.connect(**config)
+    query = conexao.cursor()
+    query.execute("use testeIntuitiveCare")
+
+    with open("Teste3/BD/operadoras_ativas.sql", "r") as script:
+        script_sql = script.read()
+        print(script_sql)
         
-    #print(f"INSERT INTO operacoes_ativas (DATA_OP, REG_ANS, CD_CONTA_CONTABIL, DESCRICAO, VL_SALDO_INICIAL, VL_SALDO_FINAL) VALUES ({linha[0]}, {linha[1]}, {linha[2]}, {linha[3]}, {linha[4]}, {linha[5]})")
-  
-    # for linha in dados:
-    #     print(f"INSERT INTO demonstracoes (Registro_ANS, CNPJ, Razao_Social, Nome_Fantasia, Modalidade, Logradouro, Numero, Complemento, Bairro, Cidade, UF, CEP, DDD, Telefone, Fax, Endereco_eletronico, Representante, Cargo_Representante, Regiao_de_Comercializacao, Data_Registro_ANS) VALUES ({linha[0]}, {linha[1]}, {linha[2]}, {linha[3]}, {linha[4]}, {linha[5]}, {linha[6]}, {linha[7]}, {linha[8]}, {linha[9]}, {linha[10]}, {linha[11]}, {linha[12]}, {linha[13]}, {linha[14]}, {linha[15]}, {linha[16]}, {linha[17]}, {linha[18]}, {linha[19]})\n\n")
-    #     query.execute(f"INSERT INTO operacoes_ativas (DATA_OP, REG_ANS, CD_CONTA_CONTABIL, DESCRICAO, VL_SALDO_INICIAL, VL_SALDO_FINAL) VALUES ({linha[0]}, {linha[1]}, {linha[2]}, {linha[3]}, {linha[4]}, {linha[5]})")
+    query.execute(script_sql)
+    conexao.commit()
+    query.close()
+    conexao.close()
 
-    # Converter a lista de listas em uma lista de tuplas
-        dados_tuplas = [tuple(linha) for linha in dados]
+def tabela_operadorasAtivas_csv(arquivos, config, batch_size=10000):
+    conexao = mysql.connector.connect(**config)
+    query = conexao.cursor()
+    query.execute("use testeIntuitiveCare")
 
-        # Executar a consulta usando executemany
-        query.executemany("""
-            INSERT INTO operacoes_ativas (
-                DATA_OP, REG_ANS, CD_CONTA_CONTABIL, DESCRICAO, VL_SALDO_INICIAL, VL_SALDO_FINAL
-            ) VALUES (%s, %s, %s, %s, %s, %s)
-        """, dados_tuplas)
+    for nome in arquivos:
+        with open(f"Teste3/{nome}.csv", "r", encoding="latin-1") as file:
+            linhas = file.readlines()[1:]
+            for i in range(0, len(linhas), batch_size):
+                batch = linhas[i:i + batch_size]
+                dados = [linha.strip().split(';') for linha in batch]
+                dados = [[valor.replace('"', '') for valor in linha] for linha in dados]
+                dados_tuplas = [tuple(linha) for linha in dados]
 
+                query.executemany("""
+                    INSERT IGNORE INTO operacoes_ativas (
+                        DATA_OP, REG_ANS, CD_CONTA_CONTABIL, DESCRICAO, VL_SALDO_INICIAL, VL_SALDO_FINAL
+                    ) VALUES (%s, %s, %s, %s, %s, %s)
+                """, dados_tuplas)
 
+                conexao.commit()
 
-conexao.commit()
-query.close()
-conexao.close()
+    query.close()
+    conexao.close()  
+
+#----------------------------------------------------            
+try:
+    arquivos = ["1T2022", "2T2022", "3T2022", "4T2022", "1T2023", "2T2023", "3T2023"]
+    config = {"host": "127.0.0.1","user": "root","password": "","connect_timeout": 5000}
+    conexao = mysql.connector.connect(**config)
+
+    query = conexao.cursor()
+    query.execute("CREATE DATABASE IF NOT EXISTS testeIntuitiveCare")
+    
+    tabela_demonstracoes_script(config)
+    tabela_demonstracoes_csv(config)
+    tabela_operadorasAtivas_script(config)
+    tabela_operadorasAtivas_csv(arquivos,config)
+    
+except mysql.connector.Error as err:
+    print(f"Erro ao conectar ao MySQL: {err}")
+finally:
+    if 'conexao' in locals() and conexao.is_connected():
+        query.close()
+        conexao.close()
 
 
 
